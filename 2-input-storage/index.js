67ab6600ea8c8ee7bab1c3908bca2df6132697d3
Mridul <mridulgain@gmail.com>
2025-04-04 09:22:49 +0530

    maintain rating history

--- 2-input-storage/index.js
+++ 2-input-storage/index.js
@@ -1,26 +1,47 @@
-var userRating = 1;
-var userName = "Guest";
+function getList(key, newList=[]){
+    storedList = sessionStorage.getItem(key);
+    if (storedList) {
+        try {
+            newList = JSON.parse(storedList);
+            // if (!Array.isArray(userList)) userList = [];
+        } catch (e) {
+            console.warn("Failed to parse :", key, e);
+            newList = [];
+        }
+    }
+    return newList;
+}
 
-// string to array
-userList = sessionStorage.getItem("userList");
-userList = userList.split(",");
+function updateList(key, element, list=[]){
+    list = getList(key, list);
+    list.push(element);
+    sessionStorage.setItem(key, JSON.stringify(list));
+    return list;
+}
+
 console.log("userList data type:", typeof userList);
 
 console.log("userRating datatype:", typeof userRating);
 document.getElementById("submit").onclick = function() {
-    userName = document.getElementById("name").value;
+    userName = document.getElementById("name").value || "Guest" ;
     console.log("username data type:", typeof userName);
     sessionStorage.setItem("userName", userName);
-    userList.push(userName);
-    sessionStorage.setItem("userList", userList);
     document.getElementById("header").innerText = `Hello ${userName}!`;
+    // update list
+    userList = updateList("userList", userName);
+    console.log("userRating list",  userList);
     
-    userRating = document.getElementById("rating").value;
+    userRating = document.getElementById("rating").value || 1;
     console.log(typeof userRating);
     sessionStorage.setItem("userRating", userRating);
-    document.getElementById("rating").innerText = `Your rating is ${userRating}!`;
+    document.getElementById("ratingDisplay").innerText = `Your rating is ${userRating}!`;
+    // update rating list
+    ratingList = updateList("ratingList", userRating);
+    console.log("userRating list",  ratingList);
 }
 
 // show avg rating
-// fix user rating undefined error
-// fix user list empty element @ 0
+// {
+//     username: "qwerty",
+//     rating: 5,
+// }
