f2ed829a1b0f6eabf2129a2e660fcc3377a3085d
Abhijai Singh <83866393+AbhijaiSingh151202@users.noreply.github.com>
2025-04-07 08:41:48 +0530

    avg-rating

--- 2-input-storage/index.js
+++ 2-input-storage/index.js
@@ -20,9 +20,16 @@ function updateList(key, element, list=[]){
 }
 
 console.log("userList data type:", typeof userList);
-
 console.log("userRating datatype:", typeof userRating);
+
+
+// let selectedRating = 0
+
+
+
+
 document.getElementById("submit").onclick = function() {
+    
     userName = document.getElementById("name").value || "Guest" ;
     console.log("username data type:", typeof userName);
     sessionStorage.setItem("userName", userName);
@@ -31,13 +38,32 @@ document.getElementById("submit").onclick = function() {
     userList = updateList("userList", userName);
     console.log("userRating list",  userList);
     
-    userRating = document.getElementById("rating").value || 1;
-    console.log(typeof userRating);
-    sessionStorage.setItem("userRating", userRating);
-    document.getElementById("ratingDisplay").innerText = `Your rating is ${userRating}!`;
-    // update rating list
-    ratingList = updateList("ratingList", userRating);
-    console.log("userRating list",  ratingList);
+    userRating = Number(document.getElementById("rating").value);
+    if (!isNaN(userRating) && userRating >= 1 && userRating <= 5) {
+         
+        console.log(typeof userRating);
+        sessionStorage.setItem("userRating", userRating);
+        document.getElementById("ratingDisplay").innerText = `Your rating is ${userRating}!`;
+        // update rating list
+        ratingList = updateList("ratingList", userRating);
+        console.log("userRating list",  ratingList);
+        showAvgRating();
+    } else {
+        alert("Please enter a rating between 1 and 5.");
+    }
+}
+
+ratingValue = document.getElementById(avgRating)
+function showAvgRating(){
+    let ratingList = getList("ratingList");
+    let sum = 0;
+    for (let i = 0; i < ratingList.length; i++){
+        sum += Number(ratingList[i]);
+    }
+    console.log(sum)
+    let avg = (sum / ratingList.length).toFixed(2);
+    console.log("Average rating:", avg);
+    document.getElementById("avgRating").innerText = `Average rating is: ${avg}!`;
 }
 
 // show avg rating
